43cfdb45f756e9f005d03fea40158958ace571f9
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2024-07-28 15:52:19 +0300

    Clean up processResults

--- static/js/files.js
+++ static/js/files.js
@@ -3,7 +3,7 @@ export async function readFile(fileHandle) {
     return new Promise(async (resolve) => {
         const file = await fileHandle.getFile();
         const reader = new FileReader();
-        reader.readAsText(file);
         reader.addEventListener("load", async () => resolve(reader.result));
+        reader.readAsText(file);
     });
 }
--- static/js/results.js
+++ static/js/results.js
@@ -4,32 +4,40 @@ export async function processResults(event, documentsDirectoryHandle, onSuccess)
     const outDir = await documentsDirectoryHandle.getDirectoryHandle("out");
     const raceOut = await outDir.getFileHandle("race_out.json");
 
-    // TODO use readFile
+    const data = await readFile(raceOut);
+    const eventResult = JSON.parse(data);
+    const raceSession = eventResult.sessions.find((session) => session.type === 3);
+    if (!raceSession) {
+        return;
+    }
 
-    const file = await raceOut.getFile();
-    const reader = new FileReader();
-    reader.readAsText(file);
-    reader.addEventListener("load", async () => {
-        const eventResult = JSON.parse(reader.result);
-        const raceSession = eventResult.sessions.find((session) => session.type === 3);
-        if (!raceSession) {
-            return;
-        }
+    const position = raceSession.raceResult.findIndex(driverId => driverId === 0) + 1;
 
-        const position = raceSession.raceResult.findIndex(driverId => driverId === 0) + 1;
-        const result = { version: 0, position, event };
+    const result = {
+        version: 0,
+        uuid: event.uuid,
 
-        console.log("Writing dailies.jsonl");
-        const saveFileHandle = await documentsDirectoryHandle.getFileHandle("dailies.jsonl", { create: true });
-        const saveFile = await saveFileHandle.getFile();
+        level: event.level,
+        position,
+        gridSize: event.gridSize,
 
-        const writable = await saveFileHandle.createWritable({ keepExistingData: true });
-        await writable.write({ type: "seek", position: saveFile.size });
-        await writable.write(JSON.stringify(result) + "\n");
-        await writable.close();
+        series: event.series.id,
+        license: event.series.license,
 
-        onSuccess();
-    });
+        trackId: event.trackId,
+        lapCount: event.lapCount,
+    };
+
+    console.log("Writing dailies.jsonl");
+    const saveFileHandle = await documentsDirectoryHandle.getFileHandle("dailies.jsonl", { create: true });
+    const saveFile = await saveFileHandle.getFile();
+
+    const writable = await saveFileHandle.createWritable({ keepExistingData: true });
+    await writable.write({ type: "seek", position: saveFile.size });
+    await writable.write(JSON.stringify(result) + "\n");
+    await writable.close();
+
+    onSuccess();
 }
 
 export async function loadHistory(documentsDirectoryHandle) {
